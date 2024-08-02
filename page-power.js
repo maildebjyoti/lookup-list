e0a7b010304d824e9a1ed0169f2c716052f2b91b
Debjyoti Acharjee <maildebjyoti@gmail.com>
2024-08-02 10:17:21 +0800

    Js update

--- page-power.js
+++ page-power.js
@@ -1,3 +1,20 @@
+console.log('Page-power:v1');
+/*
+$.get('https://raw.githubusercontent.com/maildebjyoti/lookup-list/main/page-power.js').then(js => {
+    let scriptFunction = new Function(js);
+    scriptFunction();
+    console.log('hello hello -- page-power:js');
+});
+
+$.get('https://raw.githubusercontent.com/maildebjyoti/lookup-list/main/page-power.css').then(css => {
+	let style = document.createElement('style');
+	style.innerHTML = css;
+	document.head.appendChild(style);
+	console.log('hello hello -- page-power:css');
+});
+*/
+
+
 function pagePower(){
 	console.log('Hello: Test 123');
 	let test = document.querySelectorAll('#main-content > div.confluence-information-macro.confluence-information-macro-information.conf-macro.output-block')[0].textContent;
